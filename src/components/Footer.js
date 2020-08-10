ef2b11ea303228f4c1e1b98e6031485b7639a9b8
DannyBoyNYC <daniel.deverell@gmail.com>
2020-08-10 12:46:56 -0400

    edited footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -46,32 +46,20 @@ const Footer = () => (
     <List>
       <Item>
         <a
-          href="https://www.contentful.com/"
+          href="https://www.ppuaba.com/"
           rel="nofollow noopener noreferrer"
           target="_blank"
         >
-          <img
-            src="https://images.ctfassets.net/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg"
-            style={{ width: '100px' }}
-            alt="Powered by Contentful"
-          />
+          © Park Place Underhill Avenue Block Association
         </a>
       </Item>
       <Item>
         <a
-          href="https://github.com/ryanwiemer/gatsby-starter-gcn"
+          href="https://deverell.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          gatsby-starter-gcn
-        </a>{' '}
-        by{' '}
-        <a
-          href="https://github.com/ryanwiemer"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @ryanwiemer
+          Made with ♥️ by Daniel Deverell
         </a>
       </Item>
     </List>
